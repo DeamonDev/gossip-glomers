4d2c36da1036e1ec1a3d329068704879bc124717
deamondev <piotr.rudnicki94@protonmail.com>
2025-10-11 15:27:47 +0200

    init cache

--- grow-only-counter/main.go
+++ grow-only-counter/main.go
@@ -4,20 +4,12 @@ import (
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
-	"go.uber.org/zap"
 )
 
 func main() {
 	n := maelstrom.NewNode()
 
-	cfg := zap.NewDevelopmentConfig()
-	cfg.OutputPaths = []string{"/tmp/node.log"}
-	cfg.ErrorOutputPaths = []string{"/tmp/node_error.log"}
-
-	logger, _ := cfg.Build()
-	defer logger.Sync()
-
-	s := NewServer(n, logger)
+	s := NewServer(n)
 
 	if err := s.Run(); err != nil {
 		log.Fatal(err)
--- grow-only-counter/server.go
+++ grow-only-counter/server.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"os"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 type Server struct {
@@ -39,11 +43,17 @@ type InitMessageResponse struct {
 	Type string `json:"type"`
 }
 
-func NewServer(n *maelstrom.Node, l *zap.Logger) *Server {
-	kv := maelstrom.NewKV("counter", n)
-	s := &Server{node: n, kv: kv, logger: l}
+type LocalReadMessageResponse struct {
+	Type  string `json:"type"`
+	Value int    `json:"value"`
+}
+
+func NewServer(n *maelstrom.Node) *Server {
+	kv := maelstrom.NewSeqKV(n)
+	s := &Server{node: n, kv: kv}
 
 	n.Handle("init", s.initHandler)
+	n.Handle("local_read", s.localReadHandler)
 
 	return s
 }
@@ -54,9 +64,14 @@ func (s *Server) initHandler(msg maelstrom.Message) error {
 		return err
 	}
 
-	s.logger.Info("init message received", zap.String("node_id", body.NodeID))
-
 	s.nodeID = body.NodeID
+
+	err := initLogger(s)
+	if err != nil {
+		return err
+	}
+
+	s.logger.Info("init message received", zap.String("node_id", body.NodeID))
 
 	for _, id := range body.NodeIDs {
 		if id != s.nodeID {
@@ -64,13 +79,77 @@ func (s *Server) initHandler(msg maelstrom.Message) error {
 		}
 	}
 
-	s.logger.Info("peers discovered due to initial message: ", zap.Strings("peers", s.peers))
+	s.logger.Info("peers discovered due to initial message:", zap.Strings("peers", s.peers))
 
 	initMessageResponse := InitMessageResponse{Type: "init_ok"}
 
 	return s.node.Reply(msg, initMessageResponse)
 }
 
+func (s *Server) localReadHandler(msg maelstrom.Message) error {
+	v, err := s.kv.ReadInt(context.Background(), "counter")
+	if err != nil {
+		return err
+	}
+
+	localReadMessageResponse := LocalReadMessageResponse{Type: "local_read_ok", Value: v}
+
+	return s.node.Reply(msg, localReadMessageResponse)
+}
+
+func initLogger(s *Server) error {
+	err := os.MkdirAll("/tmp/logs", 0755)
+	if err != nil {
+		return err
+	}
+
+	logFile := fmt.Sprintf("/tmp/logs/node_%s.log", s.nodeID)
+	errLogFile := fmt.Sprintf("/tmp/logs/node_%s.err.log", s.nodeID)
+
+	f, _ := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+	ef, _ := os.OpenFile(errLogFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+
+	cfg := zap.NewDevelopmentEncoderConfig()
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(cfg),
+		zapcore.AddSync(f),
+		zap.DebugLevel,
+	)
+
+	errCore := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(cfg),
+		zapcore.AddSync(ef),
+		zap.ErrorLevel,
+	)
+
+	go initCache(s)
+
+	s.logger = zap.New(zapcore.NewTee(core, errCore))
+	return nil
+}
+
+func initCache(s *Server) {
+	err := s.kv.Write(context.Background(), "counter", 0)
+	if err != nil {
+		s.logger.Fatal("failed to initialize counter", zap.Error(err))
+	} else {
+		s.logger.Info("initialized counter for myself")
+	}
+
+	for _, peerID := range s.peers {
+		err = s.kv.Write(context.Background(), peerID, 0)
+		if err != nil {
+			s.logger.Fatal("failed to initialize counter for", zap.String("peerID", peerID), zap.Error(err))
+		} else {
+			s.logger.Info("initialized counter for", zap.String("peerID", peerID))
+		}
+	}
+}
+
 func (s *Server) Run() error {
+	defer func(logger *zap.Logger) {
+		_ = logger.Sync()
+	}(s.logger)
+
 	return s.node.Run()
 }
